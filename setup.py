4337b56615afde7a09cae8a6da75af9fd295e27d
amousavi9 <amousavy9@gmail.com>
2022-12-28 22:32:08 +0330

    setup file modified

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from images_scraper import *
 
 
 parser = argparse.ArgumentParser(description='Process command line arguments.')
-parser.add_argument("q", help="query", type=str)
-parser.add_argument("n", help="max_images", type=int)
-parser.add_argument("-e", help="search_engine", type=str, default="google")
-parser.add_argument("-t", help="delay", type=int, default=4)
-parser.add_argument("-r", help="high_reselotion", action='store_true')
+parser.add_argument("query", type=str)
+parser.add_argument("max_images", type=int)
+parser.add_argument("-e", "search_engine", type=str, default="google")
+parser.add_argument("-t", "delay", type=int, default=4)
+parser.add_argument("-r", "high_reselotion", action='store_true')
 args = parser.parse_args()
 
 
@@ -17,7 +17,7 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
 
-    scraper = Image_Scraping(query=args.q, max_images=args.n, search_engine=args.e, delay=args.t, high_reselotion=args.r)
+    scraper = Image_Scraping(query=args.query, max_images=args.max_images, search_engine=args.search_engine, delay=args.delay, high_reselotion=args.high_reselotion)
 
     urls = scraper.get_image_urls()
     scraper.download_images(urls)
